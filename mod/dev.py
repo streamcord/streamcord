b2936199c6e9d523343bde6bce81aaf3dfa612aa
kanikanikun! <32210799+Kanielyochanan@users.noreply.github.com>
2018-05-25 14:55:05 -0500

    Update dev.py

--- mod/dev.py
+++ mod/dev.py
@@ -16,26 +16,26 @@ class Dev:
 
     @commands.command()
     @commands.check(owner_only)
-    async def error(self, *args):
+    async def error(self, ctx, *args):
         raise RuntimeError()
 
     @commands.command()
     @commands.check(owner_only)
-    async def args(self, *args):
-        await self.bot.say(", ".join(args))
+    async def args(self, ctx, *args):
+        await ctx.send(", ".join(args))
 
     @commands.command()
     @commands.check(check_fail)
-    async def fail_check(self):
-        await self.bot.say("I don't know how you passed this check, but congrats! :tada:")
+    async def fail_check(self, ctx):
+        await ctx.send("I don't know how you passed this check, but congrats! :tada:")
 
     @commands.command()
     @commands.check(owner_only)
-    async def ratelimits(self):
+    async def ratelimits(self, ctx):
         m = ""
         for r in self.bot.ratelimits.keys():
             m += "**{}**: {} ({})\n".format(r, self.bot.ratelimits[r], time.strftime('%m/%d/%Y %H:%M.%S', time.localtime(self.bot.ratelimits[r])))
-        await self.bot.say(m)
+        await ctx.send(m)
 
     @commands.command(pass_context=True)
     @commands.check(owner_only)
@@ -44,7 +44,7 @@ class Dev:
         if settings.BETA:
             token = settings.BETA_TOKEN
         r = requests.post("https://discordapp.com/api/channels/{}/messages".format(channel_id), headers={"Authorization": "Bot " + token, "Content-Type": "multipart/form-data"}, files=dict(content=jsonstuff))
-        await self.bot.say("```\nPOST {0.url} {0.status_code}\n\n{1}\n```".format(r, r.json()))
+        await ctx.send("```\nPOST {0.url} {0.status_code}\n\n{1}\n```".format(r, r.json()))
 
 def setup(bot):
     bot.add_cog(Dev(bot))
