0e55f750cc43e0cba40ee6901a79477c2f72c9a5
devakira <r3aper.ow@gmail.com>
2019-08-08 08:28:38 -0400

    Delete ignored files

--- launch2.py
+++
@@ -1,7 +0,0 @@
-from bot import TwitchBot, run, settings
-run(TwitchBot(**{
-    "command_prefix": ["twbeta ", "twb>"] if settings.UseBetaBot else ["twitch ", "Twitch ", "!twitch ", "tw>"],
-    "owner_id": 236251438685093889,
-    "shard_count": 40,
-    "shard_ids": [10,11,12,13,14,15,16,17,18,19]
-}))
--- launch4.py
+++
@@ -1,7 +0,0 @@
-from bot import TwitchBot, run, settings
-run(TwitchBot(**{
-    "command_prefix": ["twbeta ", "twb>"] if settings.UseBetaBot else ["twitch ", "Twitch ", "!twitch ", "tw>"],
-    "owner_id": 236251438685093889,
-    "shard_count": 40,
-    "shard_ids": [30,31,32,33,34,35,36,37,38,39]
-}))
